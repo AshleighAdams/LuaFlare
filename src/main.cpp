e09e9dd31eb961b5f4d3f3ccdab30a264fcfae01
Kate Adams <self@kateadams.eu>
2012-01-21 22:57:24 +0000

    starting idk

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+
+
+#include <microhttpd.h>
+
+using namespace std;
+
+
+int Connection(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls)
+{
+	const char *page  = "<html><body>Hello, browser!</body></html>";
+
+	struct MHD_Response* response = MHD_create_response_from_buffer (strlen (page), (void*) page, MHD_RESPMEM_PERSISTENT);
+
+	int ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
+	MHD_destroy_response (response);
+
+	return ret;
+}
+
+
+int main ()
+{
+	printf("Loading luaserver... ");
+	
+	struct MHD_Daemon *daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, 8080, NULL, NULL, &Connection, NULL, MHD_OPTION_END);
+		
+	if(!daemon)
+	{
+		printf("\t [Fail]\n");
+		return 1;
+	}
+	
+	printf("\t [OK]\n");
+	
+	while(true)
+	{
+		getchar();
+	}
+
+	MHD_stop_daemon (daemon);
+	return 0;
+}
+
+	
+	
+	
