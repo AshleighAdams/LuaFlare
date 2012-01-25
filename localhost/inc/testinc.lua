38946cefc751b24dadd7a0c898cd4039ca4f0d10
Kate Adams <self@kateadams.eu>
2012-01-25 12:41:04 +0000

    PrintTable print function now shows what kind of function one is

--- localhost/inc/testinc.lua
+++ localhost/inc/testinc.lua
@@ -12,6 +12,17 @@ local function m_PrintTable(tbl, donetbls, depth)
 		if type (v) == "table" then
 			writef(false, "<tr>%s<td>%s:</td><td>%s</td></tr>\n", tabs, EscapeHTML(tostring(k)), EscapeHTML(tostring(v)))
 			m_PrintTable(tbl[k], donetbls, depth + 1)
+		elseif type(v) == "function" then
+			local funcname = tostring(v)
+			local info = debug.getinfo(v)
+			if info then
+				if info.what == "C" then
+					funcname = "C " .. funcname
+				else
+					funcname = "Lua " .. funcname
+				end
+			end
+			writef(false, "<tr>%s<td>%s</td><td>%s</td></tr>\n", tabs, EscapeHTML(tostring(k)), EscapeHTML(funcname))
 		else
 			writef(false, "<tr>%s<td>%s</td><td>%s</td></tr>\n", tabs, EscapeHTML(tostring(k)), EscapeHTML(tostring(v)))
 		end
