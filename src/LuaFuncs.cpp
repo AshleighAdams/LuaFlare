4879758ae25cab5dc4adb926df8ec318864a52c4
Kate Adams <self@kateadams.eu>
2012-01-25 13:13:29 +0000

    More tab fixing

--- src/LuaFuncs.cpp
+++ src/LuaFuncs.cpp
@@ -17,12 +17,12 @@ int l_EscapeHTML(lua_State* L)
 {
 	string in = luaL_checkstring(L, 1);
 	
-    string buf;
-    buf.reserve(in.size());
-    for(size_t pos = 0; pos != in.size(); ++pos)
-    {
-        switch(in[pos])
-        {
+	string buf;
+	buf.reserve(in.size());
+	for(size_t pos = 0; pos != in.size(); ++pos)
+	{
+		switch(in[pos])
+		{
 			case '&':	buf.append("&amp;");		break;
 			case '\"':	buf.append("&quot;");	break;
 			case '\'':	buf.append("&apos;");	break;
@@ -30,9 +30,9 @@ int l_EscapeHTML(lua_State* L)
 			case '>':	buf.append("&gt;");		break;
 
 			default:	buf.append(1, in[pos]);	break;
-        }
-    }
-    in.swap(buf);
+		}
+	}
+	in.swap(buf);
 	
 	lua_pushstring(L, in.c_str());
 	return 1;
@@ -48,27 +48,27 @@ void PrintTable(lua_State *L, int Depth)
 	
 	lua_pushnil(L);
 	
-    while(lua_next(L, -2) != 0)
-    {
-        if(lua_isstring(L, -1))
-          printf("%s%s = %s\n", tabs, lua_tostring(L, -2), lua_tostring(L, -1));
-        else if(lua_isnumber(L, -1))
-          printf("%s%s = %d\n", tabs, lua_tostring(L, -2), lua_tonumber(L, -1));
-        else if(lua_istable(L, -1))
+	while(lua_next(L, -2) != 0)
+	{
+		if(lua_isstring(L, -1))
+			printf("%s%s = %s\n", tabs, lua_tostring(L, -2), lua_tostring(L, -1));
+		else if(lua_isnumber(L, -1))
+			printf("%s%s = %d\n", tabs, lua_tostring(L, -2), lua_tonumber(L, -1));
+		else if(lua_istable(L, -1))
 		{
 			printf("%s%s:\n", tabs, lua_tostring(L, -2));
 			PrintTable(L, Depth + 1); // TODO: Refrence to self
 		}
 
-        lua_pop(L, 1);
-    }
-    
-    delete [] tabs;
+		lua_pop(L, 1);
+	}
+	
+	delete [] tabs;
 }
 
 int l_PrintTable(lua_State *L)
 {
-    return 0;
+	return 0;
 }
 
 int l_DirExists(lua_State *L)
