4e559a92a3d2d725d7e89fe255e111b58e649dce
Kate Adams <self@kateadams.eu>
2013-09-17 04:10:56 +0100

    Added caching support (304 Not Modified)

--- lua/ar_resources.lua
+++ lua/ar_resources.lua
@@ -13,11 +13,34 @@ local function itterate_dir(dir, callback, ...)
 	end
 end
 
+local Months = {Jan=1,Feb=2,Mar=3,Apr=4,May=5,Jun=6,Jul=7,Aug=8,Sep=9,Oct=10,Nov=11,Dec=12}
+
 local function add_resource(filename, host)
 	pattern = filename:Replace(static_dir .. host, "") -- remove our dir
 	pattern = escape.pattern(pattern)
 		
 	local function serve_file(req, res)
+		local ifmod = req:headers()["If-Modified-Since"]
+		local ours = lfs.attributes(filename, "modification")
+		
+		if ifmod then
+			local pattern = "%a+, (%d+) (%a+) (%d+) (%d+):(%d+):(%d+) (%a+)"
+			local day, month, year, hour, min, sec, tz = ifmod:match(pattern)
+
+			month = Months[month]
+			local theirs = os.time({tz=tz,day=day,month=month,year=year,hour=hour,min=min,sec=sec})
+			if theirs and ours <= theirs then
+				res:set_status(304)
+				return
+			end
+		end
+		
+		local format = "%a, %d %b %Y %X UTC"
+		local timestring = os.date(format, ours)
+		local expires = os.date(format, os.time() + 1000) -- 1k seconds
+		
+		res:set_header("Last-Modified", timestring)
+		res:set_header("Expires", expires)
 		res:set_file(filename)
 	end
 	
