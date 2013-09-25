522ffa6b9088468d6db729eaa8c52b182b2dd9db
Kate Adams <self@kateadams.eu>
2013-09-25 17:57:46 +0100

    Added build hook event, thus encapsulating pushover

--- sites/buildserver/lua/ar_buildserver.lua
+++ sites/buildserver/lua/ar_buildserver.lua
@@ -1,27 +1,9 @@
+-- Hooks:
+-- BuildServer.Built
+
 include("template_buildserver.lua")
-local ssl = require("ssl")
-ssl.https = require("ssl.https")
+
 local json = require("dkjson")
-local configor = require("configor")
-
-function pushover( request ) -- https://github.com/sweetfish/pushover-lua/blob/master/pushover.lua
-	local pushover_url = "https://api.pushover.net/1/messages.json"
-	local data_str = {}
-	for k,v in pairs(request) do
-		table.insert(data_str, tostring(k) .. "=" .. tostring(v))
-	end
-	data_str = table.concat(data_str, "&")
-	local res, code, headers, status = ssl.https.request(pushover_url, data_str)
-	if (code ~= 200) then
-		local errstr = "Error while sending request. Status code: " .. tostring(code) .. ", Body: " .. tostring(res)
-		return false, errstr
-	elseif (res ~= '{"status":1}') then
-		local errstr = "Error from pushover: " .. tostring(res)
-		return false, errstr
-	end
-	return true
-end
-
 local build_status = "" -- the current status of a build
 
 local g_print = print
@@ -114,32 +96,8 @@ local function on_update(req, res, project)
 	file:write(build_status)
 	file:close()
 	
+	hook.Call("BuildServer.Built", req, project, errcode == 0, buildtime, build_status)
 	build_status = ""
-	
-	
-	local commit = payload.head_commit
-	local ops = configor.loadfile(script.local_path("options.cfg.secret"))
-		
-	local push = {
-		token = ops.pushover.token:data(),
-		user = ops.pushover.user:data(),
-		timestamp = os.time()
-	}
-	
-	if errcode ~= 0 then
-		push.title = string.format("%s build failed", project)
-		push.priority = 2
-		push.retry = 60 * 2 -- 2min
-		push.expire = 60 * 60 * 1 -- 1 hour
-		push.message = string.format("%s pushed to %s:\n%s\nBuild failed", commit.author.name, project, commit.message)
-	else
-		push.title = string.format("%s build succeded", project)
-		push.message = string.format("%s pushed to %s:\n%s\nSuccessfully built in %f seconds.", commit.author.name, project, commit.message, 
-			math.Round(delta, 0.01))
-	end
-	
-	local suc, err = pushover(push)
-	if not suc then print(err) end
 end
 
 local function get_menu()
@@ -279,10 +237,3 @@ reqs.AddPattern("*", "/build/(*)/(*)/status", on_status)
 reqs.AddPattern("*", "/build/(*)/(*)/state%.png", on_state)
 reqs.AddPattern("*", "/build/*/", redirect_master)
 
--- ensure that the options exist
-local ops = configor.loadfile(script.local_path("options.cfg.secret"), true)	
-if ops:children().pushover == nil then
-	ops.pushover.token:set_value("enter token")
-	ops.pushover.user:set_value("enter user key")
-	configor.savefile(ops, script.local_path("options.cfg.secret"))
-end
---
+++ sites/buildserver/lua/ar_pushover.lua
@@ -0,0 +1,62 @@
+local configor = require("configor")
+local json = require("dkjson")
+local ssl = require("ssl")
+ssl.https = require("ssl.https")
+
+function pushover( request ) -- https://github.com/sweetfish/pushover-lua/blob/master/pushover.lua
+	local pushover_url = "https://api.pushover.net/1/messages.json"
+	local data_str = {}
+	for k,v in pairs(request) do
+		table.insert(data_str, tostring(k) .. "=" .. tostring(v))
+	end
+	data_str = table.concat(data_str, "&")
+	local res, code, headers, status = ssl.https.request(pushover_url, data_str)
+	if (code ~= 200) then
+		local errstr = "Error while sending request. Status code: " .. tostring(code) .. ", Body: " .. tostring(res)
+		return false, errstr
+	end
+	
+	local obj = json.decode(res)
+	if obj.status ~= 1 then
+		local errstr = "Error from pushover: " .. tostring(res)
+		return false, errstr
+	end
+	
+	return true
+end
+
+-- ensure that the options exist
+local ops = configor.loadfile(script.local_path("options.cfg.secret"), true)	
+if ops:children().pushover == nil then
+	ops.pushover.token:set_value("enter token")
+	ops.pushover.user:set_value("enter user key")
+	configor.savefile(ops, script.local_path("options.cfg.secret"))
+end
+
+local function pushover(req, project, success, buildtime, shell)
+	local commit = payload.head_commit
+	local ops = configor.loadfile(script.local_path("options.cfg.secret"))
+		
+	local push = {
+		token = ops.pushover.token:data(),
+		user = ops.pushover.user:data(),
+		timestamp = os.time()
+	}
+	
+	if errcode ~= 0 then
+		push.title = string.format("%s build failed", project)
+		push.priority = 2
+		push.retry = 60 * 2 -- 2min
+		push.expire = 60 * 60 * 1 -- 1 hour
+		push.message = string.format("%s pushed to %s:\n%s\nBuild failed", commit.author.name, project, commit.message)
+	else
+		push.title = string.format("%s build succeded", project)
+		push.message = string.format("%s pushed to %s:\n%s\nSuccessfully built in %f seconds.", commit.author.name, project, commit.message, 
+			math.Round(buildtime, 0.01))
+	end
+	
+	local suc, err = pushover(push)
+	if not suc then print(err) end
+end
+
+hook.Add("BuildServer.Built", "pushover", pushover)
