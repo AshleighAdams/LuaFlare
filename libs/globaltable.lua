634253fa9eeb0e55f2e4ece4f883f188d59d4fcc
Kate Adams <self@kateadams.eu>
2013-10-04 19:49:58 +0100

    Just some ideas for global table

---
+++ libs/globaltable.lua
@@ -0,0 +1,13 @@
+local globaltable = {}
+
+local tablemeta = {}
+
+function tablemeta:update() -- pull updates since last update time
+	local lastupdate = self._private.updatetime
+end
+
+function globaltable:ensure(name, persistant) expects("string", "boolean")
+	
+end
+
+return globaltable
