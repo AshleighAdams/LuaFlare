89ad6baf8831b523334f3a5099ede41d988a6275
Kate Adams <self@kateadams.eu>
2013-10-10 00:08:14 +0100

    Moved HTTP Range handler to response:set_file()

--- inc/response.lua
+++ inc/response.lua
@@ -41,7 +41,7 @@ function meta:clear() expects(meta)
 	self.file = nil
 end
 
-function meta:set_file(path, from, to) expects(meta, "string")
+function meta:set_file(path) expects(meta, "string")
 	local file = io.open(path, "rb")
 	
 	if not file then
@@ -55,21 +55,37 @@ function meta:set_file(path, from, to) expects(meta, "string")
 	self._reply = file:read("*all")
 	file:close()
 	
-	if from ~= nil then
-		local len = self._reply:len()		
-		if len < from or (to ~= nil and len < to) then
-			-- we can't serve this
-			self:set_status(httpstatus.fromstring("Requested Range Not Satisfiable"))
-			-- tell them how long the file is
-			self:set_header("Content-Range", "bytes */" .. len)
-			self._reply = "" -- nulify the response
-		else
-			if to == nil then to = len end
-			
-			self:set_status(httpstatus.fromstring("Partial Content"))
-			-- subtract one to convert from 1 index to 0 index; C "a[n]" == Lua "a[n + 1]"
-			self:set_header("Content-Range", string.format("bytes %i-%i/%i", from - 1, to - 1, len))
-			self._reply = self._reply:sub(from, to)
+	do -- support for HTTP Range
+		local req = self:request()
+		local headers = req:headers()
+		
+		local range_from, range_to = nil, nil
+		if headers.Range ~= nil then
+			if headers.Range:Trim() == "" then
+				res:set_header("Accept-Ranges", "bytes") -- let them know that we can accept ranges
+			else
+				local from, to = string.match(headers.Range, "bytes=(%d+)-(%d*)")
+				local len = self._reply:len()
+				
+				print("client wants range " .. from .. " to " .. tostring(to))
+				from = tonumber(from) or 0
+				to = tonumber(to) or len - 1
+				
+				-- okay, serve the ranged file (if we can)
+				if len <= from  or len <= to then
+					-- we can't serve this
+					self:set_status(httpstatus.fromstring("Requested Range Not Satisfiable"))
+					-- tell them how long the file is
+					self:set_header("Content-Range", "bytes */" .. len)
+					self._reply = "" -- nulify the response
+				else
+					self:set_status(httpstatus.fromstring("Partial Content"))
+					self:set_header("Content-Range", string.format("bytes %i-%i/%i", from, to, len))
+					
+					-- add one to convert from 0 index to 1 index; C "a[n]" == Lua "a[n + 1]"
+					self._reply = self._reply:sub(from + 1, to + 1)
+				end
+			end
 		end
 	end
 	
@@ -88,7 +104,7 @@ function meta:send() expects(meta)
 	
 	self:set_header("Content-Length", self._reply:len())
 	
-	local tosend = "HTTP/1.1 " .. tostring(self._status) .. (httpstatus.tostring(self._status) or "") .. "\n"
+	local tosend = "HTTP/1.1 " .. tostring(self._status) .. " " .. (httpstatus.tostring(self._status) or "") .. "\n"
 	for k,v in pairs(self._headers) do
 		tosend = tosend .. tostring(k) .. ": " .. tostring(v) .. "\n"
 	end
