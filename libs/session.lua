5e2c4874bfc2a8df19f084507146bdb609eb1f63
Kate Adams <self@kateadams.eu>
2013-11-21 02:14:12 +0000

    Session improvments Sessions with different names may not conflict (session name is combined with sessionid in filename) Peers may not decide their own session ID When generating, it makes sure the session ID is unique

--- libs/session.lua
+++ libs/session.lua
@@ -27,24 +27,38 @@ function session.get(req, res, session_name)
 		end
 	end
 
-	if id == nil then
-		id = random_string(32)
-		res:set_cookie(session_name, id)
-	end
-
 	local ret = setmetatable({}, meta._meta)
-	ret:construct(id)
+	ret:construct(req, res, session_name, id)
 	return ret
 end
 
-function meta:construct(id)
+function meta:construct(req, res, session_name, id)
+	if id == nil then		
+		while true do -- generate one
+			id = random_string(32)
+			if table.load("sessions/" .. session_name .. "_" .. id) == nil then
+				break
+			end
+		end
+
+		print(req:peer() .. " generated a new sesion id: " .. id)
+		table.save({}, "sessions/" .. session_name .. "_" .. id)
+		res:set_cookie(session_name, id)
+	end
+
 	self._id = id
-	self._data = table.load("sessions/" .. id) or {}
+	self._session_name = session_name
+	self._data = table.load("sessions/" .. session_name .. "_" .. id)
+
+	if self._data == nil then
+		print(req:peer() .. " sent a none-existing (expired?) session id!")
+		self:construct(req, res, session_name, nil) -- force the generation of a new session
+	end
 end
 
 function meta:save()
 	util.EnsurePath("sessions/")
-	table.save(self:data(), "sessions/" .. self:id())
+	table.save(self:data(), "sessions/" .. self._session_name .. "_" .. self:id())
 end
 
 function meta:id()
