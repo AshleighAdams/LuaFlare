15c2c7d24bad3f8e6d56827a0caf551f4c55f604
Kate Adams <self@kateadams.eu>
2014-12-07 14:30:43 +0000

    string.format on request failure print

--- lua/ar_defaulterrors.lua
+++ lua/ar_defaulterrors.lua
@@ -7,7 +7,7 @@ local lfs = require("lfs")
 local template = include("template-error.lua")
 
 local function on_error(why, request, response)
-	print("request failed: %s: %s %s", why.type, request:url(), why.message or "")
+	print(string.format("fail: %s: %s %s", why.type, request:url(), why.message or ""))
 end
 hook.add("Error", "log errors", on_error)
 
