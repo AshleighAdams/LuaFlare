e66e4c07ee27cccc236cdb929a3223a82105bca0
Kate Adams <self@kateadams.eu>
2015-01-09 18:34:53 +0000

    virtualfilesystem: added `ls()` to list directories and files.

--- libs/luaflare/virtualfilesystem.lua
+++ libs/luaflare/virtualfilesystem.lua
@@ -35,4 +35,47 @@ end
 function vfs.mount(string path)
 end
 
+function vfs.ls(string path, options)
+	options = options or {}
+	local type = options.type
+	local recursive = options.recursive
+	local f = options.tester
+
+	local ret = {}
+	for file in lfs.dir(path) do
+		if file ~= "." and file ~= ".." then
+			local fullfile = string.format("%s/%s", path, file)
+			local atts = lfs.attributes(fullfile)
+
+			local add = true
+
+			--- test the path against the filters
+			if type and type ~= atts.mode then
+				add = false
+			end
+
+			--- test against tester (along with what WOULD be, return nil for what would be)
+			if f then
+				local newadd = f(fullfile, options, atts, add)
+				if newadd ~= nil then add = newadd end
+			end
+
+			-- finally, if we made it through everything, add the file
+			if add then
+				table.insert(ret, fullfile)
+			end
+
+			if recursive and atts.mode == "directory" then
+				local toappend = vfs.ls(fullfile, options)
+				for k,v in ipairs(toappend) do
+					table.insert(ret, v)
+				end
+			end
+
+		end
+	end
+
+	return ret
+end
+
 return vfs
--- thirdparty/docs/virtualfilesystem.md
+++ thirdparty/docs/virtualfilesystem.md
@@ -6,3 +6,13 @@
 
 Translates site relative file locations relative to the current working directory.
 
+## `array vfs.ls(string path, table options = {})`
+
+Returns a list of files and folders.
+
+List of valid options:
+
+ - `type`: The type (mode) of the file **must** match this.
+           The type of file may be of either `directory`, `file`, `link`, `socket`, `named pipe`, `char device`, `block device`, and `other`.
+ - `recursive`: When encountering another directory, should we recurse into it?
+ - `tester`: A function to test each file.  The arguments passed are: `string file, table options, table attributes, boolean default`.
