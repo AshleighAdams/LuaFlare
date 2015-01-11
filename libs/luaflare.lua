b9c9ec4049dba02b43db14f833ff1a9ae44096d0
Kate Adams <self@kateadams.eu>
2015-01-11 12:14:15 +0000

    luaflare lib ACR warning squelched.

--- libs/luaflare.lua
+++ libs/luaflare.lua
@@ -1,15 +1,7 @@
-return {
-	_VERSION     = "LuaFlare git",
-	config_path  = os.getenv("LUAFLARE_CFG_DIR") or ".",
-	lib_path     = os.getenv("LUAFLARE_LIB_DIR") or ".",
-	--[[hook         = require("luaflare.hook"),
-	hosts        = require("luaflare.hosts"),
-	httpstatus   = require("luaflare.httpstatus"),
-	mimetypes    = require("luaflare.mimetypes"),
-	scheduler    = require("luaflare.scheduler"),
-	session      = require("luaflare.session"),
-	tags         = require("luaflare.tags"),
-	threadpool   = require("luaflare.threadpool"),
-	util         = require("luaflare.util"),
-	websocket    = require("luaflare.websocket"),]]
-}
+local luaflare = {}
+
+luaflare._VERSION     = "LuaFlare git"
+luaflare.config_path  = os.getenv("LUAFLARE_CFG_DIR") or "."
+luaflare.lib_path     = os.getenv("LUAFLARE_LIB_DIR") or "."
+
+return luaflare
